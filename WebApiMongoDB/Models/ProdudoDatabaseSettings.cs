12e4d44fb331a87ef7013a9099edd9ae049f15b0
IlkaCarmo <ilka.scarmo@gmail.com>
2024-01-11 22:28:30 -0300

    projeto iniciado

---
+++ WebApiMongoDB/Models/ProdudoDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiMongoDB.Models
+{
+    public class ProdudoDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null;
+        public string DataBaseName { get; set; } = null;
+        public string ProdutoCollectionName { get; set; } = null;
+    }
+}
